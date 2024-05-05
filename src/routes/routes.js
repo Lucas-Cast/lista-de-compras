090a05446fd8c27367da030132cfcd68eb63a079
Lucas Castelano <100870230+Lucas-Cast@users.noreply.github.com>
2024-05-05 15:11:54 -0300

    feat:Adiciona rota para o index.html

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
+const path = require('path')
 const express = require('express')
 const productController = require('../controllers/product.controllers')
 
 const router = express.Router()
+router.use(express.static(path.join(__dirname, '..', 'views')))
 
+router.get('/', (req, res) => res.render(path.join(__dirname, '..', 'views')))
 router.post('/saveProduct', productController.saveProductController)
 router.get('/findProducts', productController.findProductsController)
 router.delete('/deleteProduct', productController.deleteProductController)
