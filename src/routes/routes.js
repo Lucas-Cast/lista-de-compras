3bacd0bd138705c4ce232dfcdac15e55ba8cc1ab
Lucas Castelano <100870230+Lucas-Cast@users.noreply.github.com>
2024-05-09 19:05:04 -0300

    feat: Adiciona rota para a documentação

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const swaggerUI = require('swagger-ui-express')
 const productController = require('../controllers/product.controllers')
+const swaggerDocs = require('../../swagger.json')
 
 const router = express.Router()
 router.use(express.static(path.join(__dirname, '..', 'views')))
-
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 router.get('/', (req, res) => res.render(path.join(__dirname, '..', 'views')))
 router.post('/saveProduct', productController.saveProductController)
 router.get('/findProducts', productController.findProductsController)
